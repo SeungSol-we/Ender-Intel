3aa8cb652645a24dabdee692d99a0557bd8ecc2c
JangSeung <164428037+Jangseun@users.noreply.github.com>
2026-03-26 00:21:36 +0900

    일단 대충 구성

---
+++ front/app/(tabs)/ai.jsx
@@ -0,0 +1,12 @@
+import { Pressable, Text, View } from 'react-native';
+
+
+const AiScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffe5ed' }}>
+            <Text>Hello 여긴 ai 관련 스크린인데 일단 test!</Text>
+        </View>
+    );
+};
+
+export default AiScreen;
